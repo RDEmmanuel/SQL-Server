afe6957d80450d124145195eb19dc14eeeea878a
Emmanuel Ruiz Diaz <109686587+RDEmmanuel@users.noreply.github.com>
2022-11-05 13:28:25 -0300

    Modificación del where

--- Anexos/SQL_Ejericio_5.sql
+++ Anexos/SQL_Ejericio_5.sql
@@ -2,7 +2,8 @@ USE base_consorcio
 
 --Ejercicio 5
 /*Mostrar los datos del administrador correspondiente al consorcio que tenga menor 
-	gasto acumulado en el año (2015) en concepto de 'servicios'*/
+	gasto acumulado en el aÃ±o (2015) en concepto de 'servicios'
+*/
 SELECT top (1) WITH TIES
 	c.idadmin as 'ID Admin',
 	MIN(a.apeynom) as 'Apellido y Nombre',
@@ -17,15 +18,19 @@ INNER JOIN gasto g
 	ON c.idconsorcio = g.idconsorcio
 	AND c.idlocalidad = g.idlocalidad
 	AND c.idprovincia = g.idprovincia
-	WHERE YEAR(g.fechapago) = 2015 AND g.idtipogasto = 5
+INNER JOIN tipogasto tg
+	ON g.idtipogasto = tg.idtipogasto
+	WHERE YEAR(g.fechapago) = 2015 AND tg.descripcion LIKE 'servicio%'
 GROUP BY c.idadmin
 ORDER BY COUNT(*) ASC
 
 
-/*Cantidad de gastos que cumplen con la condición de: año 2015 y tipo de gasto 1(servicios)*/
+/*Cantidad de gastos que cumplen con la condiciÃ³n de: aÃ±o 2015 y tipo de gasto 1(servicios)*/
 SELECT count(*) as 'Cantidad' FROM consorcio c
 INNER JOIN gasto g
 	ON c.idconsorcio = g.idconsorcio
 	AND c.idlocalidad = g.idlocalidad
 	AND c.idprovincia = g.idprovincia
-	WHERE YEAR(g.fechapago) = 2015 and g.idtipogasto = 1
+INNER JOIN tipogasto tg
+	ON g.idtipogasto = tg.idtipogasto
+	WHERE YEAR(g.fechapago) = 2015 and tg.descripcion LIKE 'servicio%'
